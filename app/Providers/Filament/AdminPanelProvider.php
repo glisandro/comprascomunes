355e7633f11cfe0691f1dd90899e20bb080f0ed9
vglobal <81919725+vglobal@users.noreply.github.com>
2024-06-29 12:01:26 -0300

    Fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('/')
             ->login(\Filament\Pages\Auth\Login::class)
             ->maxContentWidth(MaxWidth::Full)
             ->colors([
