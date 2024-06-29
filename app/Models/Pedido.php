b6eb53896eb633504da901986bf36c0fb1bb3629
vglobal <81919725+vglobal@users.noreply.github.com>
2024-06-29 12:11:39 -0300

    Fix

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -39,6 +39,6 @@ class Pedido extends Model
 
     public function pedidodetalle()
     {
-        return $this->hasMany(pedidodetalle::class);
+        return $this->hasMany(Pedidodetalle::class);
     }
 }
